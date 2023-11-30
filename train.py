54cd697b62f96b83f2354a1eaa4b596a8483076e
gdudovitch <gdudovitch@snapchat.com>
2023-11-30 12:06:36 +0200

    fix

--- train.py
+++ train.py
@@ -112,7 +112,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         loss = 0
         # iterate over batch
         for ind in range(len(viewpoint_cams)):
-            viewpoint_cam = {viewpoint_cams[k][ind] for k in viewpoint_cams.keys()}
+            viewpoint_cam = {k: viewpoint_cams[k][ind] for k in viewpoint_cams.keys()}
             viewpoint_cam = EasyCamera(**viewpoint_cam)
 
             # Render
